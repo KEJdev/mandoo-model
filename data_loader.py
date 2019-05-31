e71c642d48cef323717fe5e6a6faecc46f5eb1ff
EUNJI KIM <kej12516@gmail.com>
2019-05-31 23:53:49 +0900

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -143,4 +143,34 @@ def convert_to_query_db_data_fixed_window(img_list, label_list, input_size, num_
             reference_img.append(img)
     return queries, references, queries_img, reference_img
 
+def convert_to_query_db_data(img_list, label_list, input_size, num_classes, max_ref_count, debug):
+    """ load image with labels from filename"""
+    label_reference_cnt = {}
+    label_visit = []
+    queries = []
+    queries_img = []
+    references = []
+    reference_img = []
+    if not debug: 
+        labels = range(0, num_classes, 7)
+    else:
+        labels = range(0, 15)
+    print("query_num")
+    print(len(labels))
+    for i, (img, label) in enumerate(zip(img_list, label_list)):
+        key = "/" + str(label) + "@" + str(i) + ".jpg"
+        if label not in label_visit and label in labels:
+            queries.append(key)
+            img = image_load(img, img_size=input_size[:2])
+            queries_img.append(img)
+            label_visit.append(label)
+        elif label in label_visit:
+            if (label in label_reference_cnt.keys()) and label_reference_cnt[label] > max_ref_count: 
+                continue
+            else:
+                label_reference_cnt[label] = label_reference_cnt.get(label, 0) + 1
+            references.append(key)
+            img = image_load(img, img_size=input_size[:2])
+            reference_img.append(img)
+    return queries, references, queries_img, reference_img
 
