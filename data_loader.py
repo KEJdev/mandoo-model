4484ce342c236118769fd934a380369e9edcb4b2
EUNJI KIM <kej12516@gmail.com>
2019-05-31 23:54:29 +0900

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -174,3 +174,32 @@ def convert_to_query_db_data(img_list, label_list, input_size, num_classes, max_
             reference_img.append(img)
     return queries, references, queries_img, reference_img
 
+def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
+  """Compute the union of the current variables and checkpoint variables."""
+  assignment_map = {}
+  initialized_variable_names = {}
+
+  name_to_variable = collections.OrderedDict()
+  for var in tvars:
+    name = var.name
+    m = re.match("^(.*):\\d+$", name)
+    if m is not None:
+      name = m.group(1)
+      name_list = name.split('/')
+      if name_list[0] == 'Nasnet_block':
+          name = '/'.join(name_list[1:])
+          name += '/'
+    name_to_variable[name] = var
+
+  init_vars = tf.train.list_variables(init_checkpoint)
+
+  assignment_map = collections.OrderedDict()
+  for x in init_vars:
+    (name, var) = (x[0], x[1])
+    if name not in name_to_variable:
+      continue
+    assignment_map[name] = name
+    initialized_variable_names[name] = 1
+    initialized_variable_names[name + ":0"] = 1
+
+  return (assignment_map, initialized_variable_names)
